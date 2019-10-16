aab2b49a56d8a4c67571d3f2fad6f36ddada3522
eduardo.reis <eduardo.reis@dbserver.com.br>
2019-10-16 13:31:49 -0300

    Estado e cidade

---
+++ src/main/java/com/dbserver/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.dbserver.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dbserver.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+
+}
