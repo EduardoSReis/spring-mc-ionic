1bf3454e477095ca44ac389a786386cbc1cd2bc3
eduardo.reis <eduardo.reis@dbserver.com.br>
2019-10-17 15:54:27 -0300

    Listando categorias com DTO

--- src/main/java/com/dbserver/services/CategoriaService.java
+++ src/main/java/com/dbserver/services/CategoriaService.java
@@ -1,14 +1,11 @@
 package com.dbserver.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.dbserver.domain.Categoria;
 import com.dbserver.repositories.CategoriaRepository;
@@ -17,34 +14,37 @@ import com.dbserver.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		}
-	
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
-	public Categoria update (Categoria obj) {
+
+	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
-	public void delete (Integer id) {
+
+	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
-	} catch (DataIntegrityViolationException e) {
-		throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
-		
-	}
-		}
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 
+		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 }
