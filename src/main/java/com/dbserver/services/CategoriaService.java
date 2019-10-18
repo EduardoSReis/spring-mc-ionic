7d77c16e361c1c1f940cf786b4543d791e9de6c2
eduardo.reis <eduardo.reis@dbserver.com.br>
2019-10-18 09:34:44 -0300

    Validação sintática com Bean validation

--- src/main/java/com/dbserver/services/CategoriaService.java
+++ src/main/java/com/dbserver/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.dbserver.domain.Categoria;
+import com.dbserver.dto.CategoriaDTO;
 import com.dbserver.repositories.CategoriaRepository;
 import com.dbserver.services.exceptions.DataIntegrityException;
 import com.dbserver.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
